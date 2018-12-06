89d6a5037426e68c44589578a282700757f54e21
Simon Moulard <simon.moulard@laposte.net>
2018-12-06 22:00:46 +0100

    meilleure chevre

--- main.js
+++ main.js
@@ -1,46 +1,55 @@
-function draw() {
-  var canvas = document.getElementById('canvas');
-  if (canvas.getContext) {
-    var ctx = canvas.getContext('2d');
-	
-	ctx.fillStyle = "yellow";
-	ctx.fillRect(0, 0, 500, 500);
-	/*
-	ctx.fillStyle = "black";
-    ctx.beginPath();
-    ctx.moveTo(91, 96);
-    ctx.bezierCurveTo(88, 96, 87, 99, 87, 101);
-    ctx.bezierCurveTo(87, 103, 88, 106, 91, 106);
-    ctx.bezierCurveTo(94, 106, 95, 103, 95, 101);
-    ctx.bezierCurveTo(95, 99, 94, 96, 91, 96);
-    ctx.moveTo(103, 96);
-    ctx.bezierCurveTo(100, 96, 99, 99, 99, 101);
-    ctx.bezierCurveTo(99, 103, 100, 106, 103, 106);
-    ctx.bezierCurveTo(106, 106, 107, 103, 107, 101);
-    ctx.bezierCurveTo(107, 99, 106, 96, 103, 96);
-    ctx.fill();
-	*/
-  }
+
+function draw_goat(ctx, img, center, howbig) {
+	let width = img.width * howbig;
+	let height = img.height * howbig;
+	ctx.drawImage(img, center.x - width / 2, center.y - height / 2, width, height);
 }
 
-function draw_goat() {
+function draw_goats(ctx, img, direction, reduction, norme, count_goats, origine, first_size) {
+	let point = {x: origine.x, y: origine.y};
+	let size = first_size;
+	
+	for (var i = 0; i < count_goats; i++) {
+		draw_goat(ctx, img, point, size);
+		point = {x: point.x + direction.x * norme, y: point.y + direction.y * norme};
+		size = size * reduction;
+	}
+}
+
+var img = new Image();
+img.onload = function() {
 	var canvas = document.getElementById('canvas');
 	var ctx = canvas.getContext('2d');
-	
-	var img = new Image();
-	img.onload = function() {
-		ctx.drawImage(img, 0, 0, 150, 150);
-		//ctx.beginPath();
-		//ctx.moveTo(30, 96);
-		//ctx.lineTo(70, 66);
-		//ctx.lineTo(103, 76);
-		//ctx.lineTo(170, 15);
-		//ctx.stroke();
-	};
-	img.src = 'chevre_transparente.png';
-}
 
-draw();
-draw_goat();
+	ctx.fillStyle = "yellow";
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
+	
+	var direction = {x: 0, y: 0};
+	var reduction = .75;
+	var norme = 20;
+	var count_goats = 16;
+	var origine = {x: canvas.width / 2, y: canvas.width / 2};
+	var first_size = 5;
+	
+	draw_goats(ctx, img, direction, reduction, norme, count_goats, origine, first_size);
+	
+	canvas.onclick = function(e) {
+		console.log(e);
+		let x = e.clientX - canvas.width / 2;
+		let y = e.clientY - canvas.height / 2;
+		let norme = Math.sqrt(x * x + y * y);
+		let direction = {x: x/norme, y: y/norme};
+		
+		draw_goats(ctx, img, direction, reduction, norme, count_goats, origine, first_size);
+	};
+	
+}
+img.src = "chevre_transparente3.png";
+
+
+
+
+
+
 
 
