0073f268a4055e2e8aaf9a57dd8bd65a65fc0212
Simon Moulard <simon.moulard@laposte.net>
2018-12-07 02:07:59 +0100

    Death

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 
+var image_urls = ["resource/image/chevre_transparente3.png", "resource/image/chevre_transparente3_feu.png"];
+const hit_box_width = 60;
+const hit_box_height = 60;
+
 class Goat
 {
 	constructor(x, y)
@@ -6,11 +10,30 @@ class Goat
 		this.x = x;
 		this.y = y;
 		this.age = 0;
+		this.state = 0;
+	}
+	
+	reset()
+	{
+		this.age = 0;
+		this.state = 0;
 	}
 	
 	get_older()
 	{
 		this.age++;
+		if (this.age == 20)
+			this.state = 1;
+
+	}
+	
+	is_dead() {
+		return this.age > 50;
+	}
+	
+	is_hit(x, y)
+	{
+		return Math.abs(x - this.x) <= hit_box_width && Math.abs(y - this.y) <= hit_box_height;
 	}
 }
 
@@ -25,14 +48,22 @@ class Game
 		this.goats = [];
 		this.goat_image_scale = 0.2;
 
-		this.goat_image = new Image();
-		this.goat_image.src = "resource/image/chevre_transparente3.png";
-		
-		this.burning_goat_image = new Image();
-		this.burning_goat_image.src = "resource/image/chevre_transparente3_feu.png";
+		this.goat_images = image_urls.map((str)=>{ let img = new Image(); img.src = str; return img; });
 
 		this.canvas.addEventListener("click", (event)=>{
-			this.goats.push(new Goat(event.offsetX, event.offsetY));
+			let found_goat = null;
+			for (let goat of this.goats) {
+				if (goat.is_hit(event.offsetX,event.offsetY)) {
+					found_goat = goat;
+					break;
+				}
+			}
+			
+			if (found_goat === null)
+				this.goats.push(new Goat(event.offsetX, event.offsetY));
+			else
+				found_goat.reset();
+			
 		});
 	}
 
@@ -52,25 +83,24 @@ class Game
 	
 	draw_goat(goat)
 	{
-		let width = this.goat_image.width*this.goat_image_scale;
-		let height = this.goat_image.height*this.goat_image_scale;
-		this.context.drawImage((goat.age < 10 ? this.goat_image : this.burning_goat_image), goat.x - width / 2, goat.y - height / 2, width, height);
+		let width = this.goat_images[0].width*this.goat_image_scale;
+		let height = this.goat_images[0].height*this.goat_image_scale;
+		this.context.drawImage(this.goat_images[goat.state], goat.x - width / 2, goat.y - height / 2, width, height);
 	}
 	
 	draw_all_goats()
 	{
 		for (let goat of this.goats) {
-			
 			this.draw_goat(goat);
 		}
 	}
 
 	update()
 	{
+		this.goats = this.goats.filter( (goat) => { return !goat.is_dead(); });
 		for (let goat of this.goats) {
 			goat.get_older();
 		}
-		
 	}
 }
 
