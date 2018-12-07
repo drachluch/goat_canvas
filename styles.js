48a12753bdea92f22a35bb39993cc178747100c9
Simon Moulard <simon.moulard@laposte.net>
2018-12-07 05:26:39 +0100

    d

--- styles.js
+++ styles.js
@@ -1,29 +1,3 @@
-// var is_night_mode = false;
-// var style = document.getElementById("style");
-
-// function update_css()
-// {
-    // if(is_night_mode)
-    // {
-        // style.href = "style_black.css";
-    // }
-    // else
-    // {
-        // style.href = "style.css";
-    // }
-// }
-
-// function switch_css()
-// {
-    // is_night_mode = !is_night_mode;
-    // update_css();
-// }
-
-// var switch_button = document.getElementById("style_switch");
-// switch_button.addEventListener("click", switch_css);
-
-// update_css();
-
 var is_night_mode = false;
 var style = document.getElementById("style");
 
