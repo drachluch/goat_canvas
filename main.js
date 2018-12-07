7364dbefbb9c686715cec0373ffb2586ca48afee
Simon Moulard <simon.moulard@laposte.net>
2018-12-07 03:44:59 +0100

    d

--- main.js
+++ main.js
@@ -5,10 +5,15 @@ var image_urls = [
 	"resource/image/chevre_transparente3_feu_corne.png",
 	"resource/image/chevre_transparente3_feu_petite_corne.png",
 	"resource/image/chevre_transparente3_feu_corne2.png",
-	"resource/image/chevre_transparente3_feu_grande_corne.png"
+	"resource/image/chevre_transparente3_feu_grande_corne.png",
+	"resource/image/chevre_transparente3_rouge2.png"
 	];
+var transition_age = [20, 10, 10, 10, 10, 10, 20];
 const hit_box_width = 60;
 const hit_box_height = 60;
+
+if (image_urls.length != transition_age.length)
+	console.log("it does not work this way.");
 
 class Goat
 {
@@ -29,7 +34,7 @@ class Goat
 	get_older()
 	{
 		this.age++;
-		if (this.age == 20 && this.state < 5) {
+		if (this.age >= 10 && this.state < 6) {
 			this.age = 0;
 			this.state++;
 		}
@@ -37,7 +42,7 @@ class Goat
 	}
 	
 	is_dead() {
-		return this.age > 50;
+		return this.age >= 20;
 	}
 	
 	is_hit(x, y)
@@ -52,8 +57,16 @@ class Game
 	{
 		this.canvas = document.getElementById('canvas');
 		
-		this.audio_agony = document.getElementById('audio_agony');
-		this.audio_cooling = document.getElementById('audio_cooling');
+		this.audio_agony_s = [];
+		this.audio_cooling_s = [];
+		
+		for (let i = 0; i < 5; i++)
+			this.audio_agony_s.push(document.getElementById('audio_agony'+i));
+		for (let i = 0; i < 10; i++)
+			this.audio_cooling_s.push(document.getElementById('audio_cooling'+i));
+		
+		this.current_audio_agony = 0;
+		this.current_audio_cooling = 0;
 		
 		this.canvas.width = window.innerWidth;
 		this.canvas.height = window.innerHeight;
@@ -75,9 +88,11 @@ class Game
 			if (found_goat === null)
 				this.goats.push(new Goat(event.offsetX, event.offsetY));
 			else {
+				if (found_goat.state != 0) {
+					this.audio_cooling_s[this.current_audio_cooling].play();
+					this.current_audio_cooling = (this.current_audio_cooling + 1) % this.audio_cooling_s.length;
+				}
 				found_goat.reset();
-				this.audio_cooling.currentTime = 0;
-				this.audio_cooling.play();
 			}
 			
 		});
@@ -117,8 +132,8 @@ class Game
 		this.goats = this.goats.filter( (goat) => { return !goat.is_dead(); });
 		
 		if (count != this.goats.length) {
-			this.audio_agony.currentTime = 0;
-			this.audio_agony.play();
+			this.audio_agony_s[this.current_audio_agony].play();
+			this.current_audio_agony = (this.current_audio_agony + 1) % this.audio_agony_s.length;
 		}
 		
 		for (let goat of this.goats) {
